2f5630d033765d8189d9984d6d07d2d78e7ca097
Kazuma <arctogalidian@gmail.com>
2019-04-07 11:05:37 +0900

    最小化ボタンで画像ページも消えるようにした
    説明！

--- image.js
+++ image.js
@@ -1,3 +1,7 @@
-var ImageBox = document.createElement('img');
-Explorer.appendChild(ImageBox);
-ImageBox.src = "img/" + current[0].title + ".png";
+var Content = document.createElement('div');
+Explorer.appendChild(Content);
+Content.setAttribute("id","content");
+
+	var ImageBox = document.createElement('img');
+	Content.appendChild(ImageBox);
+	ImageBox.src = "img/" + current[0].title + ".png";
